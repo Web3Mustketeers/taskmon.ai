6f37902314dcf81a7a697958ba21596876ac402e
TAK_Ryuji <kozayupapa@gmail.com>
2023-07-06 20:06:29 +0900

    refactor transfer

--- Sandbox/Solana/src/test.ts
+++ Sandbox/Solana/src/test.ts
@@ -1,12 +1,19 @@
 
 import * as fs from 'fs';
 import secret = require('/Users/takehararyuuji/ryu-solana-wallet/my-keypair.json');
-import { Keypair } from "@solana/web3.js";
+import { Keypair,PublicKey } from "@solana/web3.js";
 import {mint} from "./mint_nft"
+import {transfer_nft,get_nft_detail} from "./transfer_nft"
 
-const WALLET = Keypair.fromSecretKey(new Uint8Array(secret));
-let config = JSON.parse(fs.readFileSync("./src/nft_config/config_fp_h.json", "utf-8"))
-config.attributes[0].value=5;
-console.log(config);
-mint(config,WALLET);
-
+(async () => {
+  const wallet = Keypair.fromSecretKey(new Uint8Array(secret));
+  const toKey = new PublicKey("8qEt8FhkBvcBAcZrk6P9nyCpjC9zwZhrw17qCea54BkW");
+  
+  let config = JSON.parse(fs.readFileSync("./src/nft_config/config_fp_h.json", "utf-8"))
+  //config.attributes[0].value=9;
+  console.log(config);
+  const tokenMint = await mint(config,wallet);
+  console.log(tokenMint);
+  let ret = await transfer_nft(tokenMint,toKey,wallet);
+  console.log(ret);
+})();
--- Sandbox/Solana/src/transfer_nft.ts
+++ Sandbox/Solana/src/transfer_nft.ts
@@ -1,23 +1,23 @@
-import { Metaplex, keypairIdentity, bundlrStorage, toMetaplexFile, toBigNumber, tryOrNull } from "@metaplex-foundation/js";
 import { Connection, Transaction, PublicKey,sendAndConfirmTransaction } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, getOrCreateAssociatedTokenAccount,createTransferInstruction, getAccount, getAssociatedTokenAddress } from "@solana/spl-token";
 
 const connection = new Connection(
   "https://api.devnet.solana.com",
   "confirmed"
   );
-  export async function get_nft_detail(nftOrSft,toPubKey, wallet) {
-    let ata = await getAssociatedTokenAddress(
-      nftOrSft, // mint
-      wallet.publicKey // owner
-    );
-    console.log(`ATA: ${ata.toBase58()}`);    
-    let tokenAccount = await getAccount(connection, ata);    
-    console.log("tokenAccount",tokenAccount);
-  };
 
-  export async function transfer_nft(nftOrSft,toPubKey, wallet) {
-    //let tmp = await createAssociatedTokenAccount(
-  //  connection,wallet,nftOrSft,toPubKey);
+export async function get_nft_detail(nftOrSft, accountpubkey) {
+  let ata = await getAssociatedTokenAddress(
+    nftOrSft, // mint
+    accountpubkey // owner
+  );
+  console.log(`ATA: ${ata.toBase58()}`);    
+  let tokenAccount = await getAccount(connection, ata);    
+  console.log("tokenAccount",tokenAccount);
+  return tokenAccount;
+};
+
+export async function transfer_nft(nftOrSft,toPubKey, wallet) {
   const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
     connection,
     wallet,
@@ -33,7 +33,7 @@ const connection = new Connection(
 
   const receiverAccount = await connection.getAccountInfo(associatedDestinationTokenAddr.address);
   console.log(receiverAccount);
-  
+
   let transaction = new Transaction();
   const transfer = createTransferInstruction(
     fromTokenAccount.address,
@@ -44,35 +44,14 @@ const connection = new Connection(
   transaction.add(transfer);
   transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
   //console.log(transaction.instructions[0].keys);
-  await sendAndConfirmTransaction(connection, transaction, [wallet]);  
+  return await sendAndConfirmTransaction(connection, transaction, [wallet]);  
 }
 
-export async function transfer_nft2(nftOrSft,owner,toPubKey, WALLET) {
-
-  let METAPLEX = Metaplex.make(connection)
-  .use(keypairIdentity(WALLET))
-  .use(bundlrStorage({
-      address: 'https://devnet.bundlr.network',
-      providerUrl: "https://api.devnet.solana.com",
-      timeout: 60000,
-  }));  
-  await METAPLEX.nfts().transfer({
-      nftOrSft,
-      authority: owner,
-      fromOwner: owner.publicKey,
-      toOwner: toPubKey
-  });
-}
-
-import secret = require('/Users/takehararyuuji/ryu-solana-wallet/my-keypair.json');
-import { Keypair } from "@solana/web3.js";
-import { createAssociatedTokenAccount, getOrCreateAssociatedTokenAccount,createTransferInstruction, getAccount, getAssociatedTokenAddress } from "@solana/spl-token";
-
+/*
 const wallet = Keypair.fromSecretKey(new Uint8Array(secret));
 const toKey = new PublicKey("8qEt8FhkBvcBAcZrk6P9nyCpjC9zwZhrw17qCea54BkW");
 const tokenMint = new PublicKey("8A5UtHiWpa87kXgbPwznhJ8zWKzdnQuzeGcjE8yTp4fS");
 //const sourceNft = new PublicKey("5yVjDN4673TqRvrXM1rgjcqmVUU3AdtRsFcCDrBC4iFX");
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
 
 const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID: PublicKey = new PublicKey(
@@ -89,4 +68,4 @@ const tmp = PublicKey.findProgramAddressSync(
 console.log("Sync:",tmp);
 (async () => {
   await transfer_nft(tokenMint,toKey,wallet);
-})();
+})();*/
