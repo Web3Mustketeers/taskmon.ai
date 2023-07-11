10f7e382296e81dc0c0378dbdd01e2f770672612
Hiro <laciferin@gmail.com>
2023-07-12 02:39:59 +0530

    fix: app module
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,7 +3,35 @@ import { TasksModule } from './tasks/tasks.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeOrmConfig } from './config/typeorm.config'
 import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
+
+let mode = process.env.MODE
+let envFile=".env"
+
+switch (mode){
+  case "test":
+    envFile=".env.test"
+    break
+  default:
+    mode = "dev"
+    envFile=".env.local"
+}
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({
+      envFilePath: [envFile, ".env.local", ".env.test", ".env.prod", ".env"],
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        MODE: Joi.string().valid("dev", "prod", "test").default("dev"),
+        PORT: Joi.number().default(1606),
+        JWT_SECRET: Joi.string(),
+      }),
+    }),
+    AuthModule,
+    TasksModule
+  ],
 })
 export class AppModule {}
